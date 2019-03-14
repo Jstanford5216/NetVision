427e263e6f7e70f41742d54efc59678aacaad2e5
Jstanford5216 <jstanford5216@gmail.com>
2019-03-14 11:04:17 +0000

    missing file

--- Express_API/server.js
+++ Express_API/server.js
@@ -11,52 +11,59 @@ app.listen(3000, () => {
 
 app.use(bodyParser.json());
 
-app.use(function(res,res,next) {
-  res.header("Access-Control-Allow-Origin","http://10.0.0.1");
+app.use(function (res, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://10.0.0.1");
   res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
 
-app.route('/api/:name').get((req, res,next) => {
-  const requestedDevice = req.params['name'];
-  var versionList = [];
-  shell.cd("/home/jason/Documents/backups");
-  versionList = shell.ls(`${requestedDevice}*`);
-  res.send(versionList);
-  });
-  
-app.route('/api/').post((req, res,next) => {
+app.route('/api/:name').get((req, res, next) => {
+
+  query = req.params['name'];
+
+  if (query == "devices") {
+  res.send("/home/jason/Documents/collection.json")
+  }
+  else {
+    const requestedDevice = req.params['name'];
+    var versionList = [];
+    shell.cd("/home/jason/Documents/backups");
+    versionList = shell.ls(`${requestedDevice}*`);
+    res.send(versionList);
+  }
+});
+
+app.route('/api/').post((req, res, next) => {
   var command;
   if (req.body.version != null || req.body.version != "") {
     command = new Ansible.Playbook().playbook(req.body.command)
-                                      .variables({ selectedHost: req.body.device, selectedVersion: req.body.version });
+      .variables({ selectedHost: req.body.device, selectedVersion: req.body.version });
   }
-  else{
-  //Create command
-  command = new Ansible.Playbook().playbook(req.body.command)
-                                      .variables({ selectedHost: req.body.device });
+  else {
+    //Create command
+    command = new Ansible.Playbook().playbook(req.body.command)
+      .variables({ selectedHost: req.body.device });
   }
   //Set inventory
   command.inventory('hosts');
 
   //live result
-  command.on('stdout', function(data) { console.log(data.toString()); });
-  command.on('stderr', function(data) { console.log(data.toString()); }); 
-  
-  //Execute command
-  var promise = command.exec({cwd:"/etc/ansible/playbooks"});
+  command.on('stdout', function (data) { console.log(data.toString()); });
+  command.on('stderr', function (data) { console.log(data.toString()); });
 
-  promise.then(function(result)
-  {
-    res.send(201, `${result.code},${result.output}`);
+  //Execute command
+  var promise = command.exec({ cwd: "/etc/ansible/playbooks" });
+
+  promise.then(function (result) {
+    res.send(201, result.code);
   });
 });
 
-app.route('/api/:name').put((req, res,next) => {
+app.route('/api/:name').put((req, res, next) => {
   res.send(200, req.body);
 });
 
-app.route('/api/:name').delete((req, res,next) => {
+app.route('/api/:name').delete((req, res, next) => {
   res.sendStatus(204);
 });
